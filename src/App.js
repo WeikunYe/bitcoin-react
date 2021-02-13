389ab498d81375624589938c151750d91fb761c1
Weikun Ye <weikunye1225@gmail.com>
2021-02-14 09:47:38 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends React.Component {
+  state = {
+    btc_info: {}
+  }
+  componentDidMount() {
+    axios.get(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=NVFT11VSGYOKN6QW`)
+      .then(res => {
+        const btc_info = res.data['Realtime Currency Exchange Rate'];
+        console.log(btc_info);
+        this.setState({ btc_info });
+      })
+  }
+  getToday(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-wrapper">
+          <p>
+            Bitcoin
+          </p>
+          <p>USD ${Math.round(this.state.btc_info['5. Exchange Rate']).toLocaleString()}</p>
+          <p className="App-date">{this.getToday()}</p>
+        </div>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
